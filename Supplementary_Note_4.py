4215322b77789a34b98ccf9b01cd694cb29c28d0
klimaj <klimaj@uw.edu>
2020-01-05 12:49:58 -0800

    Update file paths

--- Supplementary_Note_4.py
+++ Supplementary_Note_4.py
@@ -5,6 +5,7 @@ import pyrosetta.distributed.io as io
 import pyrosetta.distributed.packed_pose as packed_pose
 import re
 
+
 def circularly_permute(pose, new_Nterm_pdb_res):
     
     lines_list = io.to_pdbstring(pose).split("\n")
@@ -37,77 +38,83 @@ def circularly_permute(pose, new_Nterm_pdb_res):
     
     return out_pose
 
-notebook_flags = """
--out:level 300 
--ignore_unrecognized_res 1
--ex2 1
--ex1aro 1
--holes:dalphaball DAlphaBall.gcc
--extra_res_fa HBI.fa.params
--extra_res_cen HBI.cen.params
-"""
-pyrosetta.distributed.dask.init_notebook(notebook_flags)
 
-new_Nterm_pdb_res = [35, 63, 89, 106]
+def main():
 
-for pdb in ["mFAP2a", "mFAP2b"]:
+    notebook_flags = """
+    -out:level 300 
+    -ignore_unrecognized_res 1
+    -ex2 1
+    -ex1aro 1
+    -extra_res_fa HBI.fa.params
+    -extra_res_cen HBI.cen.params
+    """
+    pyrosetta.distributed.dask.init_notebook(notebook_flags)
 
-    pose = pyrosetta.io.pose_from_file(f"{pdb}.pdb")
+    new_Nterm_pdb_res = [35, 63, 89, 106]
 
-    for new_Nterm in new_Nterm_pdb_res:
-        
-        tmp_pose = pose.clone()
-        out_pose = circularly_permute(tmp_pose, new_Nterm)
-        
-        scorefxn = pyrosetta.create_score_function("ref2015")
-        
-        Nterminus_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector("1A-2A")
-        Cterminus_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector("114A-115A")
-        termini_selector = pyrosetta.rosetta.core.select.residue_selector.OrResidueSelector(Nterminus_selector, Cterminus_selector)
-        not_termini_selector = pyrosetta.rosetta.core.select.residue_selector.NotResidueSelector(termini_selector)
-        tf = pyrosetta.rosetta.core.pack.task.TaskFactory()
-        tf.push_back(pyrosetta.rosetta.core.pack.task.operation.InitializeFromCommandline())
-        tf.push_back(pyrosetta.rosetta.core.pack.task.operation.IncludeCurrent())
-        tf.push_back(pyrosetta.rosetta.core.pack.task.operation.NoRepackDisulfides())
-        tf.push_back(pyrosetta.rosetta.core.pack.task.operation.OperateOnResidueSubset(
-            pyrosetta.rosetta.core.pack.task.operation.PreventRepackingRLT(), not_termini_selector))
-        mmf = pyrosetta.rosetta.core.select.movemap.MoveMapFactory()
-        mmf.all_bb(setting=False)
-        mmf.all_bondangles(setting=False)
-        mmf.all_bondlengths(setting=False)
-        mmf.all_chi(setting=False)
-        mmf.all_jumps(setting=False)
-        mmf.set_cartesian(setting=False)
-        enable = pyrosetta.rosetta.core.select.movemap.move_map_action.mm_enable
-        mmf.add_chi_action(action=enable, selector=termini_selector)
-        mmf.add_bb_action(action=enable, selector=termini_selector)
-        fast_relax = pyrosetta.rosetta.protocols.relax.FastRelax(scorefxn_in=scorefxn, standard_repeats=1)
-        fast_relax.cartesian(False)
-        fast_relax.set_task_factory(tf)
-        fast_relax.set_movemap_factory(mmf)
-        fast_relax.minimize_bond_angles(False)
-        fast_relax.minimize_bond_lengths(False)
-        fast_relax.min_type("dfpmin_armijo_nonmonotone")
-        fast_relax.apply(out_pose)
-        
-        outdir = "circularly_permuted_relaxed_pdbs"
-        if not os.path.isdir(outdir):
-            os.mkdir(outdir)
-        new_name = "cp" + str(new_Nterm) + "-" + str(new_Nterm - 1) + f"_{pdb}.pdb"
-        
-        # Output pose with a TER line at cutpoint
-        cutpoint = int(re.search("SRAAQLLPGTWQ", out_pose.sequence()).start() + 1)
-        lines_list = io.to_pdbstring(out_pose).split("\n")
-        cont = True
-        new_lines_list = []
-        for line in lines_list:
-            if line.startswith("ATOM") or line.startswith("HETATM"):
-                pdb_res = int(line[22:26].split()[-1])
-                if (pdb_res == cutpoint) and cont:
-                    new_lines_list.append("TER")
-                    cont = False
-                new_lines_list.append(line)
-            elif line.startswith("TER"):
-                new_lines_list.append(line)
-        with open(os.path.join(outdir, new_name), "w") as f:
-            f.write("\n".join(new_lines_list))
+    for pdb in ["mFAP2a", "mFAP2b"]:
+
+        pose = pyrosetta.io.pose_from_file(os.path.join("pdbs/mFAP_pdbs", f"{pdb}.pdb"))
+
+        for new_Nterm in new_Nterm_pdb_res:
+            
+            tmp_pose = pose.clone()
+            out_pose = circularly_permute(tmp_pose, new_Nterm)
+            
+            scorefxn = pyrosetta.create_score_function("ref2015")
+            
+            Nterminus_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector("1A-2A")
+            Cterminus_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector("114A-115A")
+            termini_selector = pyrosetta.rosetta.core.select.residue_selector.OrResidueSelector(Nterminus_selector, Cterminus_selector)
+            not_termini_selector = pyrosetta.rosetta.core.select.residue_selector.NotResidueSelector(termini_selector)
+            tf = pyrosetta.rosetta.core.pack.task.TaskFactory()
+            tf.push_back(pyrosetta.rosetta.core.pack.task.operation.InitializeFromCommandline())
+            tf.push_back(pyrosetta.rosetta.core.pack.task.operation.IncludeCurrent())
+            tf.push_back(pyrosetta.rosetta.core.pack.task.operation.NoRepackDisulfides())
+            tf.push_back(pyrosetta.rosetta.core.pack.task.operation.OperateOnResidueSubset(
+                pyrosetta.rosetta.core.pack.task.operation.PreventRepackingRLT(), not_termini_selector))
+            mmf = pyrosetta.rosetta.core.select.movemap.MoveMapFactory()
+            mmf.all_bb(setting=False)
+            mmf.all_bondangles(setting=False)
+            mmf.all_bondlengths(setting=False)
+            mmf.all_chi(setting=False)
+            mmf.all_jumps(setting=False)
+            mmf.set_cartesian(setting=False)
+            enable = pyrosetta.rosetta.core.select.movemap.move_map_action.mm_enable
+            mmf.add_chi_action(action=enable, selector=termini_selector)
+            mmf.add_bb_action(action=enable, selector=termini_selector)
+            fast_relax = pyrosetta.rosetta.protocols.relax.FastRelax(scorefxn_in=scorefxn, standard_repeats=1)
+            fast_relax.cartesian(False)
+            fast_relax.set_task_factory(tf)
+            fast_relax.set_movemap_factory(mmf)
+            fast_relax.minimize_bond_angles(False)
+            fast_relax.minimize_bond_lengths(False)
+            fast_relax.min_type("dfpmin_armijo_nonmonotone")
+            fast_relax.apply(out_pose)
+            
+            outdir = "circularly_permuted_relaxed_pdbs"
+            if not os.path.isdir(outdir):
+                os.mkdir(outdir)
+            new_name = "cp" + str(new_Nterm) + "-" + str(new_Nterm - 1) + f"_{pdb}.pdb"
+            
+            # Output pose with a TER line at cutpoint
+            cutpoint = int(re.search("SRAAQLLPGTWQ", out_pose.sequence()).start() + 1)
+            lines_list = io.to_pdbstring(out_pose).split("\n")
+            cont = True
+            new_lines_list = []
+            for line in lines_list:
+                if line.startswith("ATOM") or line.startswith("HETATM"):
+                    pdb_res = int(line[22:26].split()[-1])
+                    if (pdb_res == cutpoint) and cont:
+                        new_lines_list.append("TER")
+                        cont = False
+                    new_lines_list.append(line)
+                elif line.startswith("TER"):
+                    new_lines_list.append(line)
+            with open(os.path.join(outdir, new_name), "w") as f:
+                f.write("\n".join(new_lines_list))
+
+
+if __name__ == "__main__":
+    main()
